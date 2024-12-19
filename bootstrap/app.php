792cb484a0ada89171d873c5371c34a43a7c9d8e
Lucas Andrin <lucas.andrin.programmer@gmail.com>
2024-12-19 16:53:36 -0300

    feat: Install Tenancy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,31 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
+use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        // health: '/up',
+        // web: __DIR__.'/../routes/web.php',
+        // api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        using: function () {
+            Route::prefix('api')->middleware('api')->group(function () {
+                /** @var string[] */
+                $centralDomains = config('tenancy.central_domains');
+
+                foreach ($centralDomains as $domain) {
+                    Route::domain($domain)
+                        ->group(base_path('routes/api.php'));
+                }
+
+                Route::middleware([
+                    InitializeTenancyByDomain::class,
+                    PreventAccessFromCentralDomains::class,
+                ])->group(base_path('routes/tenant.php'));
+            });
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
